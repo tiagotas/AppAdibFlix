d88409224d0314046b0bd1d9c652894733104711
Usuario <Usuario@CPD02-M00>
2022-04-13 14:29:13 -0300

    Adicionar arquivos de projeto.

---
+++ AppAdibFlix/AppAdibFlix/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppAdibFlix
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnBianca.Source = ImageSource.FromResource("AppAdibFlix.Posters.bianca.png");
+        }
+
+        private void btnBianca_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PushAsync(new Categorias.Aventura());
+
+            } catch(Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+    }
+}
